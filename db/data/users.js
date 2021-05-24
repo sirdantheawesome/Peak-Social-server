7595f994fd2dd5b0d2fc63e73058db81287e9c09
Craigc <craigclement@hotmail.com>
2021-05-24 12:12:08 +0100

    craig profile

--- db/data/users.js
+++ db/data/users.js
@@ -16,6 +16,15 @@ export default [
     image: 'https://instagram.flhr3-4.fna.fbcdn.net/v/t51.2885-19/s320x320/90252192_146512903332422_4855195378321981440_n.jpg?tp=1&_nc_ht=instagram.flhr3-4.fna.fbcdn.net&_nc_ohc=ONFuSlbBHP0AX9JXsJV&edm=ABfd0MgBAAAA&ccb=7-4&oh=9ae41dbe4c82ad0af2efd95f83b5099d&oe=60AF40EF&_nc_sid=7bff83',
     summary: 'Dan is Dan',
     peekcoin: 1000,
+  }, 
+  {
+    username: 'Craig',
+    email: 'craig@email',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: 'https://instagram.flhr3-4.fna.fbcdn.net/v/t51.2885-19/s320x320/90252192_146512903332422_4855195378321981440_n.jpg?tp=1&_nc_ht=instagram.flhr3-4.fna.fbcdn.net&_nc_ohc=ONFuSlbBHP0AX9JXsJV&edm=ABfd0MgBAAAA&ccb=7-4&oh=9ae41dbe4c82ad0af2efd95f83b5099d&oe=60AF40EF&_nc_sid=7bff83',
+    summary: 'Dan is Dan',
+    peekcoin: 1000,
   }
 ]
 
