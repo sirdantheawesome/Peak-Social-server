4b824b28716427bae953d7d0ea617a8985b068b9
sirdan <danielsdfullerton@gmail.com>
2021-05-24 15:45:29 +0100

    createPost working

--- db/data/posts.js
+++ db/data/posts.js
@@ -3,7 +3,6 @@ export default [
     title: 'yooooo',
     text: 'i guess this is the text',
     image: 'https://static.independent.co.uk/2021/02/05/07/dogecoin%20elon%20musk.jpg?width=1200&auto=webp&quality=75',
-
   }
 ]
 
