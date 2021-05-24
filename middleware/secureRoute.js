cfe4e7e4546357c728a119fbef16a24498b11052
sirdan <danielsdfullerton@gmail.com>
2021-05-24 15:08:47 +0100

    fixed updateProfile

--- middleware/secureRoute.js
+++ middleware/secureRoute.js
@@ -8,23 +8,28 @@ import { secret } from '../config/environment.js'
 export default function secureRoute(req, res, next) {
 
   const rawToken = req.headers.authorization
-  if (!rawToken || !rawToken.startsWith('Bearer')) {
+  if (!rawToken || !rawToken.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Unauthorized' })
   }
   const token = rawToken.replace('Bearer ', '')
   console.log('Token:', token)
 
   jwt.verify(token, secret, async (err, payload) => {
+    console.log('Starting Verification...')
     if (err) {
       console.log('Unauthorized Bad Token')
       return res.status(401).json({ message: 'Unauthorized' })
     }
-    const user = await User.findById(payload.profileId)
+    const user = await User.findById(payload.userId)
+    console.log('Valid Token, Checking user:')
+    console.log(token)
     if (!user) {
       console.log('Unauthorized Bad User')
       return res.status(401).json({ message: 'Unauthorized' })
     }
+    console.log('Valid user: ', user)
     req.currentUser = user
+
     next()
   })
 }
