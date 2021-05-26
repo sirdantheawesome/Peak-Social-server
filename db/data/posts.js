1929dfcf5a054fee84215b0d8b54aefdd8b63704
sirdan <danielsdfullerton@gmail.com>
2021-05-26 12:05:25 +0100

    seed changes to fix profile pictures

--- db/data/posts.js
+++ db/data/posts.js
@@ -3,6 +3,16 @@ export default [
     title: 'yooooo',
     text: 'i guess this is the text',
     image: 'https://static.independent.co.uk/2021/02/05/07/dogecoin%20elon%20musk.jpg?width=1200&auto=webp&quality=75',
+  },
+  {
+    title: 'Posts are cool',
+    text: 'Big text thingy',
+    image: 'https://i.kym-cdn.com/photos/images/newsfeed/001/878/329/dfa.jpg',
+  },
+  {
+    title: 'I want to be the very best',
+    text: 'That noone ever was',
+    image: 'https://www.futuregamereleases.com/wp-content/uploads/2019/07/pokemon-new-era-ash-ketchum-hat.png',
   }
 ]
 
